8ce23bc4d2c8aa06443351bd02d8f32290ac8d76
Matthew1996i <matthew199624@gmail.com>
2019-06-04 10:12:34 -0300

    Primeira submissao git

---
+++ app.js
@@ -0,0 +1,55 @@
+// Carregando Modulos
+
+    const express = require('express')
+    const handlebars = require('express-handlebars')
+    const bodyParser = require('body-parser')
+    const app = express()
+    const admin = require('./routes/admin')
+    const path = require('path')
+    const mongoose = require('mongoose')
+    const session = require('express-session')
+    const flash = require('connect-flash')
+
+// Configurações
+    // Sessão
+        app.use(session({
+            secret: 'cursodenode',
+            resave: true,
+            saveUninitialized: true
+        }))
+        app.use(flash())
+    // Middleware
+        app.use((req, res, next)=>{
+            res.locals.success_msg = req.flash("success_msg")
+            res.locals.error_msg = req.flash("error_msg")
+            next()
+        })
+    // Body-parser
+        app.use(bodyParser.urlencoded({extended: true}))
+        app.use(bodyParser.json())
+    // Handlebars
+        app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+        app.set('view engine', 'handlebars')
+    // Mongoose
+        mongoose.Promise = global.Promise
+        mongoose.connect('mongodb://localhost/blogapp', { useNewUrlParser: true }).then(()=>{
+            console.log('Conectado ao MongoDB')
+        }).catch((err)=>{
+            console.log("Erro ao se Conectar: "+err)
+        })
+    // Public
+        app.use(express.static(path.join(__dirname, 'public')))
+// Rotas
+    app.get('/', (req, res)=>{
+        res.render('index')
+    })
+    app.get('/posts', (req, res)=>{
+        res.send('Lista de Posts: ')
+    })
+    app.use('/admin', admin)
+// Conexão
+
+const PORT = 8081
+app.listen(PORT, ()=>{
+    console.log('Servidor Online no endereço: http://localhost:'+PORT)
+})
